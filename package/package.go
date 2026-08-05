4d02783de832713711b720b698795bb0e2c993ab
jurgen.kluft <jurgen.kluft@gmail.com>
2026-08-05 08:57:41 +0800

    * adjust to changes in ccode

--- package/package.go
+++ package/package.go
@@ -3,7 +3,7 @@ package ccore
 import (
 	csdk "github.com/jurgen-kluft/csdk/package"
 	cunittest "github.com/jurgen-kluft/cunittest/package"
-	denv "github.com/jurgen-kluft/gide/denv"
+	denv "github.com/jurgen-kluft/go-ide/denv"
 )
 
 const (
